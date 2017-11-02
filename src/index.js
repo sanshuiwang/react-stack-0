499345131a2c2a73c5dc3bcd5d3d311db833d4f6
wangy <wangy@yuexing.com>
2017-11-02 18:03:58 +0800

    路由4学习

--- src/index.js
+++ src/index.js
@@ -6,26 +6,39 @@ import store from './redux/store';
 
 import Hello from 'component/Hello';
 
-import getRouter from 'router/router';
+/*初始化项目的render*/
+// import getRouter from 'router/router';
+//
+// /*初始化*/
+// renderWithHotReload(getRouter());
+//
+// /*热更新*/
+// if(module.hot){
+//   module.hot.accept('./router/router',()=>{
+//     const getRouter = require('./router/router').default;
+//     renderWithHotReload(getRouter());
+//   });
+// }
+//
+// /*Provider组件是让所有的组件可以访问到store。不用手动去传。也不用手动去监听*/
+// function renderWithHotReload(rootElement){
+//   ReactDom.render(
+//     <AppContainer>
+//       <Provider store={store}>
+//         {rootElement}
+//       </Provider>
+//     </AppContainer>,
+//     document.getElementById('app'));
+// }
 
-/*初始化*/
-renderWithHotReload(getRouter());
-
-/*热更新*/
-if(module.hot){
-  module.hot.accept('./router/router',()=>{
-    const getRouter = require('./router/router').default;
-    renderWithHotReload(getRouter());
-  });
-}
-
-/*Provider组件是让所有的组件可以访问到store。不用手动去传。也不用手动去监听*/ 
-function renderWithHotReload(rootElement){
-  ReactDom.render(
-    <AppContainer>
-      <Provider store={store}>
-        {rootElement}
-      </Provider>
-    </AppContainer>,
-    document.getElementById('app'));
-}
+/*路由引练习*/
+import BasicExample from 'router/rr4/1';
+import ParamsExample from 'router/rr4/2';
+import AuthExample from 'router/rr4/3';
+ReactDom.render(
+  <AppContainer>
+    <Provider store={store}>
+      {AuthExample()}
+    </Provider>
+  </AppContainer>,
+  document.getElementById('app'));
